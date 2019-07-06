78e1db0d1bc4a136b243af511abd20131773c95b
Cristian <cristianguerra@rocketmail.com>
2019-07-06 16:20:14 -0400

    First commit.
    (Sorry! this should've been better with more commits but it's what it is)
    Everything is very well commented 😎

---
+++ index.js
@@ -0,0 +1,103 @@
+'use strict';
+const alfy = require('alfy');
+const fs   = require('fs');
+
+// Get Local By Flywheel Json directory.
+const jsonDir = `${require('os').homedir()}/Library/Application Support/Local by Flywheel`;
+
+/**
+ * Alfred environment variables
+ * @reference https://www.alfredapp.com/help/workflows/script-environment-variables/
+ */
+const { alfred_preferences, alfred_workflow_uid } = process.env;
+
+
+/**
+ * Get Sites JSON data (Promise)
+ * @param {file} File dir.
+ */
+function getSitesData( file ) {
+	return new Promise(( resolve, reject ) => {
+		fs.readFile( file, 'utf8', function (err, data) {
+			if ( data ) {
+				resolve( JSON.parse( data ) );
+			} else {
+				reject( Error( err ) );
+			}
+		});
+	})
+}
+
+/**
+ * Add "Site Status" info (Promise)
+ * @param {SiteData} Site original data.
+ */
+function addSiteStatus( siteData ) {
+	return new Promise((resolve, reject) => {
+		fs.readFile( `${jsonDir}/site-statuses.json`, 'utf8', function (err, data) {
+			if ( data ) {
+				data = JSON.parse( data );
+				let newSiteData = siteData; // Store in memory
+				// Adding "status" object key/value to newSiteData
+				Object.keys(newSiteData).map( key => newSiteData[key]['status'] = data[key] );
+				resolve( newSiteData )
+			} else {
+				reject( Error( err ) )
+			}
+		})
+	});
+}
+
+/**
+ * Filter Site Data
+ * @param {cb} cb
+ */
+function filterSiteData( cb ) {
+
+	// Get data from sites.json (promise)
+	getSitesData( `${jsonDir}/sites.json` )
+		.then( data => {
+			// Get promise.
+			return addSiteStatus( data );
+		})
+		.then( data => {
+			// Convert object to array of objects
+			let dataArray = Object.keys(data).map((site) => data[site]);
+			// Filter sites by text input (First character only, alfy does the rest in "inputMatches")
+			cb( dataArray.filter( site => site['name'][0].toLowerCase() === alfy.input[0].toLowerCase() ) );
+		})
+}
+
+/**
+ * Map data
+ */
+filterSiteData( async data => {
+
+	const items = alfy
+	.inputMatches(data, 'name')
+	.map(site => ({
+		title: site.name,
+		subtitle: `https://${site.domain}`,
+		arg: `https://${site.domain}`,
+		mods: {
+			cmd: {
+				subtitle: 'Open Root Directory (app/public/)',
+				arg: `file://${site.path}/app/public`,
+			},
+			alt: {
+				subtitle: site.status === 'running' ? 'Open SSH' : 'Open SSH (Disabled when site is off)',
+				arg: site.status === 'running' ? 'ssh' : 'false',
+				variables: {
+					container: site.container,
+					sitename: site.name,
+				}
+			}
+		},
+		icon: {
+			path: `${alfred_preferences}/workflows/${alfred_workflow_uid}/${site.status === 'running' ? 'icon.png' : 'icon-gray.png'}`
+		},
+	}));
+
+	alfy.output(items);
+
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-local [![Build Status](https://travis-ci.org/explorador/local-by-flywheel-alfred.svg?branch=master)](https://travis-ci.org/explorador/local-by-flywheel-alfred)
+
+> Local Sites
+
+
+## Install
+
+```
+$ npm install --global alfred-local
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `local`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [“Cristian”](http://cristianguerra.com)
